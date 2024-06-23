c3e8172a848963c5721d9531dfc230cba44a538d
Udeepa_Gallage <udeepagallage@gmail.com>
2024-06-23 21:25:06 +0530

    file creation

---
+++ Prac_2/1.scala
@@ -0,0 +1,9 @@
+    def value_ass(r: Double): Double = {
+        math.Pi * r * r
+    }
+
+    def main(args: Array[String]): Unit = {
+        val r = 5
+        val area = Area_Disk(r)
+        println(s"Area of the disk with radius $r is $area")
+    }
