eaccbb7d18c7b60841cb2a9b165dfc37bcdc0d13
Udeepa_Gallage <udeepagallage@gmail.com>
2024-07-09 08:21:48 +0530

    final

--- Prac_4/3.scala
+++ Prac_4/3.scala
@@ -1,11 +1,21 @@
-object convert{
+object convert {
 
     def toUpper(str: String): String = {
-        str.toUpperCase()
+        str.map(c => if (c >= 'a' && c <= 'z') (c - 32).toChar else c)
     }
+
     def toLower(str: String): String = {
-        str.toLowerCase()
+        str.map(c => if (c >= 'A' && c <= 'Z') (c + 32).toChar else c)
     }
+
+    def substring(str: String, start: Int, end: Int): String = {
+        var result = ""
+        for (i <- start until end) {
+            result += str(i)
+        }
+        result
+    }
+
     def formatNames(name: String)(format_fun: String => String): String = {
         format_fun(name)
     }
@@ -13,13 +23,12 @@ object convert{
     def main(args: Array[String]): Unit = {
         val names = List(
             ("Benny", toUpper),
-            ("Niroshan", (s: String) => s.substring(0,2).toUpperCase() + s.substring(2)),
+            ("Niroshan", (s: String) => toUpper(substring(s, 0, 2)) + substring(s, 2, s.length)),
             ("Saman", toLower),
-            ("Kumara", (s: String) => s.substring(0,1).toUpperCase() + s.substring(1, s.length -1 ) + s.substring(s.length -1).toUpperCase())
+            ("Kumara", (s: String) => toUpper(substring(s, 0, 1)) + substring(s, 1, s.length - 1) + toUpper(substring(s, s.length - 1, s.length)))
         )
 
         names.foreach(name => println(formatNames(name._1)(name._2)))
-
     }
 
 }
