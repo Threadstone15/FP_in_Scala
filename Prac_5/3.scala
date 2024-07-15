5a13ecd318d160c94c312e3c9bd49bbb4e8a7b31
Udeepa_Gallage <udeepagallage@gmail.com>
2024-07-15 22:35:07 +0530

    fibonnaci

--- Prac_5/3.scala
+++ Prac_5/3.scala
@@ -1,17 +1,24 @@
 object fibonacci {
 
-    def first_n_fib(n: Int): List[Int] = {
-        if (n == 0) {
-            List()
-        } else {
-            List(0, 1) ++ first_n_fib(n - 1)
+    def first_n_fib(n: Int): Int = {
+        n match {
+            case 0 => 0
+            case 1 => 1
+            case _ => first_n_fib(n - 1) + first_n_fib(n - 2)
         }
+    }
+
+    def printFibonacci(n: Int): Unit = {
+    for (i <- 0 until n) {
+        println(first_n_fib(i))
+    }
     }
 
     def main(args: Array[String]): Unit = {
         println("Enter number for n: ")
         val n = scala.io.StdIn.readInt()
-        println(first_n_fib(n))
+        println(s"The first $n Fibonacci numbers are:")
+        printFibonacci(n)
     }
 
 }
