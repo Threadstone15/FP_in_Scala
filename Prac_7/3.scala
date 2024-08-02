25fa266ce1b25ea9455bd6394deb963d28fe67ff
Udeepa_Gallage <udeepagallage@gmail.com>
2024-08-02 11:39:45 +0530

    Prac_7, q3
    prime number sort

--- Prac_7/3.scala
+++ Prac_7/3.scala
@@ -1,12 +1,18 @@
 import scala.io.StdIn.readLine
 
-def filter_evn_num(num: List[Int]): List[Int] = {
-    num.filter(num => num % 2 == 0)
+object find_prime{
+
+
+    def filterPrime(numbers: List[Int]): List[Int] = {
+        numbers.filter(num => num > 1 && (num == 2 || !(2 until num).exists(i => num % i == 0)))
+    }
+
+    def main(args: Array[String]): Unit = {
+        println("Enter a list of integers separated by spaces:")
+        val input = readLine().split(" ").map(_.toInt).toList
+        val prime_num = filterPrime(input)
+        println(s"The list of prime numbers is: $prime_num")
+    }
+
 }
 
-def main(args: Array[String]): Unit = {
-    println("Enter a list of integers separated by spaces:")
-    val input = readLine().split(" ").map(_.toInt).toList
-    val evn_num = filter_evn_num(input)
-    println(s"The list of even numbers is: $evn_num")
-}
